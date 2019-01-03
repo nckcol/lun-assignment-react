25329496eb191b08d3993899d22bdd7f30941eea
Nikita Popov <nckcol@gmail.com>
2019-01-03 08:44:36 +0200

    feat: init App layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-container">
+          <div className="App-progress">
+            <button type="button">1</button>
+            <button type="button">2</button>
+            <button type="button">3</button>
+            <button type="button">4</button>
+          </div>
+          <div className="App-content">
+            <Switch>
+              <Route path="/" exact render={() => <span>Start form</span>} />
+              <Route
+                path="/location"
+                exact
+                render={() => <span>Location</span>}
+              />
+              <Route path="/social" exact render={() => <span>Social</span>} />
+              <Route
+                path="/confirm"
+                exact
+                render={() => <span>Confirm</span>}
+              />
+              <Route
+                path="/success"
+                exact
+                render={() => <span>Success!</span>}
+              />
+              <Redirect to="/" />
+            </Switch>
+          </div>
+        </div>
       </div>
     );
   }
